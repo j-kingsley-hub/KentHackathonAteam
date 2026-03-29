57e12f106b29679dd7c899c78b048d2d8b7386b9
Corbin <63320116+Tw1ZZLER@users.noreply.github.com>
2026-03-29 12:36:58 +0000

    Fix overlapping box

--- main/ui/screens/ui_screen_openai.c
+++ main/ui/screens/ui_screen_openai.c
@@ -180,7 +180,7 @@ void ui_screen_openai_screen_init(void)
     extern lv_obj_t *ui_selection_label;
     ui_selection_box = lv_obj_create(ui_screen_openai);
     lv_obj_set_width(ui_selection_box, 200);
-    lv_obj_set_height(ui_selection_box, 220);
+    lv_obj_set_height(ui_selection_box, 240);
     lv_obj_align(ui_selection_box, LV_ALIGN_TOP_RIGHT, -10, 20);
     lv_obj_set_style_bg_color(ui_selection_box, lv_color_hex(0xFFFFFF), LV_PART_MAIN | LV_STATE_DEFAULT);
     lv_obj_set_style_bg_opa(ui_selection_box, 255, LV_PART_MAIN | LV_STATE_DEFAULT);
@@ -190,7 +190,7 @@ void ui_screen_openai_screen_init(void)
 
     extern lv_obj_t *ui_selection_thumb;
     ui_selection_thumb = lv_img_create(ui_selection_box);
-    lv_obj_align(ui_selection_thumb, LV_ALIGN_BOTTOM_MID, 0, -20);
+    lv_obj_align(ui_selection_thumb, LV_ALIGN_BOTTOM_MID, 0, -5);
 
     ui_selection_label = lv_label_create(ui_selection_box);
     lv_obj_set_width(ui_selection_label, lv_pct(90));
